f7a42fdd48952b0a4381b09c48f44b11d2521d69
Javier Elias Dagobeth Montes <38734266+IngJEDagobethM@users.noreply.github.com>
2022-08-23 16:11:11 -0500

    Creación Inicial del Proyecto

---
+++ src/test/java/me/ingjedagobethm/springboot/webflux/app/SpringBootWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package me.ingjedagobethm.springboot.webflux.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebfluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
