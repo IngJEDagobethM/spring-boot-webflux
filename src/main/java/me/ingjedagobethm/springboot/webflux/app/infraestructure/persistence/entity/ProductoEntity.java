fe05919588ab06a8500ed4e69e1bdc7eba1e8d79
Javier Elias Dagobeth Montes <38734266+IngJEDagobethM@users.noreply.github.com>
2022-08-24 16:53:10 -0500

    Creando Nuevos Productos. Formulario e Integración

--- src/main/java/me/ingjedagobethm/springboot/webflux/app/infraestructure/persistence/entity/ProductoEntity.java
+++ src/main/java/me/ingjedagobethm/springboot/webflux/app/infraestructure/persistence/entity/ProductoEntity.java
@@ -3,6 +3,7 @@ package me.ingjedagobethm.springboot.webflux.app.infraestructure.persistence.ent
 import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
 
@@ -17,5 +18,6 @@ public class ProductoEntity {
     private String id;
     private String nombre;
     private Double precio;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date createAt;
 }
--- src/main/java/me/ingjedagobethm/springboot/webflux/app/infraestructure/web/controller/ProductoController.java
+++ src/main/java/me/ingjedagobethm/springboot/webflux/app/infraestructure/web/controller/ProductoController.java
@@ -8,8 +8,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.thymeleaf.spring5.context.webflux.ReactiveDataDriverContextVariable;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.time.Duration;
 
@@ -21,7 +23,7 @@ public class ProductoController {
     private final ProductHandler productHandler;
 
     @GetMapping({"/productos", "/"})
-    public String listar(Model model){
+    public Mono<String> listar(Model model){
         //Flux<ProductoEntity> productos = productHandler.execGetProducts();
         Flux<ProductoEntity> productos = productHandler.execGetProductsUpperCase();
 
@@ -31,25 +33,40 @@ public class ProductoController {
 
         model.addAttribute("productos", productos);
         model.addAttribute("titulo", "Listado Productos");
-        return "listar";
+        return Mono.just("listar");
     }
 
     @GetMapping("/productos/datadriver")
-    public String listarDataDriver(Model model){
+    public Mono<String> listarDataDriver(Model model){
         Flux<ProductoEntity> productos = productHandler.execGetProducts()
                 .delayElements(Duration.ofSeconds(2));
         // new ReactiveDataDriverContextVariable(productos, 1) es como se maneja la contrapresión (flujo, elementos_a_mostrar)
         model.addAttribute("productos", new ReactiveDataDriverContextVariable(productos, 1));
         model.addAttribute("titulo", "Listado Productos (DataDriver)");
-        return "listar";
+        return Mono.just("listar");
     }
 
     @GetMapping("/productos/chunked")
-    public String listarChunked(Model model){
+    public Mono<String> listarChunked(Model model){
         Flux<ProductoEntity> productos = productHandler.execGetProductsMasive();
 
         model.addAttribute("productos", productos);
         model.addAttribute("titulo", "Listado Productos (Chunked)");
-        return "listar";
+        return Mono.just("listar");
+    }
+
+    @GetMapping("/productos/")
+    public Mono<String> crearFormulario(Model model){
+        model.addAttribute("producto", new ProductoEntity());
+        model.addAttribute("titulo", "Nuevo Producto");
+        return Mono.just("formulario");
+    }
+
+    @PostMapping("/productos/")
+    public Mono<String> insertar(ProductoEntity producto){
+        return productHandler.execSaveProduct(producto)
+                .doOnNext(p -> log.info("Producto <".concat(producto.getId()).concat(" ").concat(p.getNombre()).concat("> insertado.")))
+                .thenReturn("redirect:/productos");
+        //      .then(Mono.just("redirect:/productos"));
     }
 }
