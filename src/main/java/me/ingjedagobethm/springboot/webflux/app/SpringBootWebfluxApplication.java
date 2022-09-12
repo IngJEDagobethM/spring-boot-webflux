32925b775a5081b3868549fc90c66fd3d9d7b633
Javier Elias Dagobeth Montes <38734266+IngJEDagobethM@users.noreply.github.com>
2022-09-12 10:14:11 -0500

    Se implementa el registro en Eureka Server

--- src/main/java/me/ingjedagobethm/springboot/webflux/app/SpringBootWebfluxApplication.java
+++ src/main/java/me/ingjedagobethm/springboot/webflux/app/SpringBootWebfluxApplication.java
@@ -10,10 +10,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import reactor.core.publisher.Flux;
 
 import java.util.Date;
 
+@EnableEurekaClient
 @RequiredArgsConstructor
 @SpringBootApplication
 public class SpringBootWebfluxApplication implements CommandLineRunner {
